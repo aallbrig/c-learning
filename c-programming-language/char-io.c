cdb11289aef9ede3925e4a1fdbb3356592aa8bb9
aallbrig <aallbrig@gmail.com>
2024-10-08 12:53:37 -0400

    exercise 1-6, 1-7

--- c-programming-language/char-io.c
+++ c-programming-language/char-io.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void main() {
 	int c;
-	while ((c = getchar()) != EOF) // != takes precedence over = (assignment); the paranthesis around `x = expression` is therefore necessary
-		putchar(c);
+	while (true) { // != takes precedence over = (assignment); the paranthesis around `x = expression` is therefore necessary
+		c = getchar();
+		if ((c != EOF) == 1)
+			putchar(c);
+		else {
+			printf("EOF signal received! %d\n", EOF);
+			break;
+		}
+	}
 }
